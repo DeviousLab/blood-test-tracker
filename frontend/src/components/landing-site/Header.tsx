85673b4b9efd82f91383937e3b35b558df13552a
DeviousLab <deviouslab@gmail.com>
2022-09-16 23:33:07 +0400

    feat: created draft Header and Hero components
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/components/landing-site/Header.tsx
+++ frontend/src/components/landing-site/Header.tsx
@@ -1,5 +1,63 @@
+import { useState } from 'react';
+import { CgClose, CgMenuRightAlt } from 'react-icons/cg';
+
+import Logo from '../../assets/images/temp-logo.svg';
+
 const Header = () => {
-  
-  return <div>Header</div>
-}
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="bg-gray-100">
+      <nav className="w-full">
+        <div className="container mx-auto flex items-center justify-around px-6">
+          <div className="flex items-center" aria-label="Home" role="img">
+            <img className="w-8 cursor-pointer" src={Logo.src} alt="logo" />
+            <p className="ml-2 font-Poppins text-base text-logo lg:ml-2 lg:text-2xl">
+              triage
+            </p>
+          </div>
+          <div>
+            <button
+              onClick={() => setShow(!show)}
+              className="text-gray-500 hover:text-gray-700 focus:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 sm:block md:hidden lg:hidden"
+            >
+              <CgMenuRightAlt className="h-8 w-8 text-secondary" />
+            </button>
+            <div
+              id="menu"
+              className={` ${show ? '' : 'hidden'} md:block lg:block `}
+            >
+              <button
+                onClick={() => setShow(!show)}
+                className={`fixed top-0 z-30 mt-6 block text-gray-500 hover:text-gray-700 focus:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 md:hidden lg:hidden`}
+              >
+                <CgClose className="h-8 w-8 text-secondary" />
+              </button>
+              <ul className="fixed inset-0 z-20 flex flex-col items-center justify-center bg-white py-10 text-3xl md:relative md:flex md:flex-row md:bg-transparent md:text-base">
+                <li className="cursor-pointer pt-10 text-base text-logo hover:text-gray-900 md:pt-0 lg:text-base">
+                  <a href="">Features</a>
+                </li>
+                <li className="cursor-pointer pt-10 text-base text-logo hover:text-gray-900 md:ml-5 md:pt-0 lg:ml-10 lg:text-base">
+                  <a href="">Process</a>
+                </li>
+                <li className="cursor-pointer pt-10 text-base text-logo hover:text-gray-900 md:ml-5 md:pt-0 lg:ml-10 lg:text-base">
+                  <a href="">Pricing</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex text-3xl">
+            <button className="hidden bg-transparent px-4 py-1 text-sm text-logo hover:text-gray-900 sm:px-8 sm:py-3 md:block lg:text-base">
+              Login
+            </button>
+            <button className="hidden rounded-3xl  border border-primary bg-transparent px-4 py-1 text-sm text-primary transition duration-300 ease-in-out hover:bg-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 sm:px-8 sm:py-3 md:block lg:text-base">
+              Sign Up 🡒
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
 export default Header;
--- frontend/src/components/landing-site/Hero.tsx
+++ frontend/src/components/landing-site/Hero.tsx
@@ -1,5 +1,53 @@
+import Dashboard from '../../assets/images/dashboard.svg';
+import LeftOverlay from '../../assets/images/LeftOverlay.svg';
+import RightOverlay from '../../assets/images/RightOverlay.svg';
+
 const Hero = () => {
-  
-  return <div>Hero</div>
-}
+  return (
+    <div className="overflow-y-hidden bg-gray-100 py-12">
+      <div className="w-full px-6">
+        <div className="container relative mx-auto mt-8 flex flex-col items-center rounded-lg bg-primary pt-12 pb-24 sm:pt-24 sm:pb-32 md:pb-48 lg:pb-56 xl:pb-64">
+          <img
+            className="absolute right-0 top-0 mr-2 mt-2 lg:mr-12 lg:mt-12"
+            src={LeftOverlay.src}
+            alt="bg"
+          />
+          <img
+            className="absolute bottom-0 left-0 ml-2 mb-2 lg:ml-12 lg:mb-12"
+            src={RightOverlay.src}
+            alt="bg"
+          />
+          <div className="mb-5 w-11/12 sm:mb-10 sm:w-2/3">
+            <h1 className="text-center font-Inter text-2xl leading-tight text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+              Track your lab results
+            </h1>
+            <p className="mt-2 text-center font-Arimo text-white">
+              If you&apos;re looking for a way to keep track of your blood test
+              results, look no further than{' '}
+              <span className="font-extrabold">triage</span>. With our
+              easy-to-use interface, you can easily track your results over time
+              and spot any trends that may emerge.
+            </p>
+          </div>
+          <div className="mb-10 flex items-center justify-center sm:mb-20">
+            <button className="rounded-3xl border bg-white px-4 py-1 text-sm text-accent transition duration-300 ease-in-out hover:border-white hover:bg-accent	hover:text-white focus:outline-none focus:ring-2 focus:ring-accent focus:ring-offset-2 sm:px-8 sm:py-3 lg:text-xl">
+              Learn more
+            </button>
+          </div>
+        </div>
+        <div className="container mx-auto -mt-20 flex justify-center sm:-mt-40 md:-mt-56">
+          <div className="relative w-11/12 sm:w-2/3">
+            <img
+              className="rounded drop-shadow-xl transition duration-300 ease-in hover:drop-shadow-2xl"
+              src={Dashboard.src}
+              alt="Sample dashboard page"
+              role="img"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Hero;
