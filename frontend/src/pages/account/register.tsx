5d7e373a04e984f76f7b15b003ba50cbc5d48371
DeviousLab <deviouslab@gmail.com>
2022-09-28 22:33:54 +0400

    feat: created draft register page
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/pages/account/register.tsx
+++ frontend/src/pages/account/register.tsx
@@ -1,6 +1,18 @@
 import type { NextPage } from 'next';
 
+import Footer from '../../components/account/Footer';
+import Header from '../../components/account/Header';
+import RegisterForm from '../../components/account/RegisterForm';
+import { Meta } from '../../layouts/Meta';
+
 const Register: NextPage = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <Meta title="triage | Register" description="Register for an account" />
+      <Header />
+      <RegisterForm />
+      <Footer />
+    </>
+  );
 };
 export default Register;
