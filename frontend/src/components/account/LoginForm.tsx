acca7ce78a0a5deacf25f348e9d6aa14ff7dc857
DeviousLab <deviouslab@gmail.com>
2022-09-28 22:55:03 +0400

    feat: created draft login page
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/components/account/LoginForm.tsx
+++ frontend/src/components/account/LoginForm.tsx
@@ -4,14 +4,14 @@ const RegisterForm = () => {
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg">
         <h1 className="text-center font-Inter text-2xl font-bold text-primary sm:text-3xl">
-          Get started today!
+          Welcome back!
         </h1>
 
         <form
           action=""
           className="mt-6 mb-0 space-y-4 rounded-lg bg-gray-50 p-8 font-Arimo text-logo"
         >
-          <p className="text-lg font-medium">Sign up for an account</p>
+          <p className="text-lg font-medium">Sign in to your account</p>
 
           <div>
             <label htmlFor="email" className="text-sm font-medium">
@@ -83,49 +83,11 @@ const RegisterForm = () => {
             </div>
           </div>
 
-          <div>
-            <label htmlFor="password" className="text-sm font-medium">
-              Confirm Password
-            </label>
-
-            <div className="relative mt-1">
-              <input
-                type="password"
-                id="password"
-                className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-                placeholder="Enter password"
-              />
-
-              <span className="absolute inset-y-0 right-4 inline-flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                  />
-                </svg>
-              </span>
-            </div>
-          </div>
-
           <button
             type="submit"
             className="block w-full rounded-lg bg-primary px-5 py-3 text-sm font-medium text-white transition-colors hover:bg-accent focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-white"
           >
-            Sign up
+            Sign in
           </button>
 
           <div className="flex items-center py-1 text-xs uppercase text-gray-400 before:mr-6 before:flex-[1_1_0%] before:border-t before:border-gray-300 after:ml-6 after:flex-[1_1_0%] after:border-t after:border-gray-300">
@@ -160,14 +122,14 @@ const RegisterForm = () => {
                   fill="#EB4335"
                 />
               </svg>
-              Sign up with Google
+              Sign in with Google
             </button>
           </div>
 
           <p className="text-center text-sm text-gray-500">
-            Already have an account?
-            <a className="ml-1 underline" href="/account">
-              Sign in here
+            Don't have an account yet?
+            <a className="ml-1 underline" href="/account/register">
+              Sign up here
             </a>
           </p>
         </form>
--- frontend/src/pages/account/index.tsx
+++ frontend/src/pages/account/index.tsx
@@ -1,6 +1,18 @@
 import type { NextPage } from 'next';
 
-const Account: NextPage = () => {
-  return <div>Login</div>;
+import Footer from '../../components/account/Footer';
+import Header from '../../components/account/Header';
+import LoginForm from '../../components/account/LoginForm';
+import { Meta } from '../../layouts/Meta';
+
+const Register: NextPage = () => {
+  return (
+    <>
+      <Meta title="triage | Login" description="Login to your account" />
+      <Header />
+      <LoginForm />
+      <Footer />
+    </>
+  );
 };
-export default Account;
+export default Register;
