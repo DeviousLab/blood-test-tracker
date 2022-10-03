c14573dde53161670dd884bfc2f7b39ffc8c5166
DeviousLab <deviouslab@gmail.com>
2022-10-04 00:39:33 +0400

    feat: added amplify auth
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    triagef65cb7e5: {
+      IdentityPoolId: 'string';
+      IdentityPoolName: 'string';
+      HostedUIDomain: 'string';
+      OAuthMetadata: 'string';
+      UserPoolId: 'string';
+      UserPoolArn: 'string';
+      UserPoolName: 'string';
+      AppClientIDWeb: 'string';
+      AppClientID: 'string';
+    };
+  };
+};
