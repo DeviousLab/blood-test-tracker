255106e0a608d555d328e70e50e6250c2db1a280
DeviousLab <deviouslab@gmail.com>
2022-10-10 01:21:44 +0400

    refactor: adjusted location of amplify config
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/global.css';
 
+import { Amplify } from 'aws-amplify';
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import awsconfig from '../aws-exports';
+
+Amplify.configure({ ...awsconfig, ssr: true });
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Toaster position="bottom-right" reverseOrder={false} />
+  </>
 );
 
 export default MyApp;
