a3156ac16769787756fc67bd4f895375f97d3664
DeviousLab <deviouslab@gmail.com>
2022-10-11 22:57:06 +0400

    fix: removed aws-exports from gitignore (https://github.com/aws-amplify/docs/issues/503)
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,10 +4,11 @@ import { Amplify } from 'aws-amplify';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 
-Amplify.configure({
-  ...JSON.parse(process.env.NEXT_PUBLIC_AWS_CONFIG as string),
-  ssr: true,
-});
+// @ts-ignore
+// eslint-disable-next-line import/extensions
+import awsmobile from '../aws-exports.js';
+
+Amplify.configure({ ...awsmobile, ssr: true });
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
